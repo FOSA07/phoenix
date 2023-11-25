fcce3cdb283eebf13fbeca9f36ce040bd83232ab
Faleti Oluwaseyi Samuel <faletioluwaseyisam@gmail.com>
2023-11-25 17:56:05 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
+import Welcome from './Pages/Welcome';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        {/* <Route path="/signUp" element={<SignUp />} /> */}
+        {/* <Route path="/signIn" element={<SignIn />} /> */}
+        {/* <Route path="/home" element={<Landing />} /> */}
+        {/* <Route path="/home/*" element={<Landing />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
+
 export default App;
+
+// backgroundImage: `url(${image1})`,
